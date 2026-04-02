862fadd24ef1a20e4f5495b4439af56ac949e72d
Sabyrka <sabyrka23@gmail.com>
2026-04-02 23:30:48 +0500

    feat: add isDisabled prop to Button component for better accessibility

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import styles from './Button.module.css'
 
-export const Button = ({ children, type = 'button' }) => {
+export const Button = ({ children, isDisabled, type = 'button' }) => {
   return (
-    <button className={styles.button} type={type}>
+    <button className={styles.button} type={type} disabled={isDisabled}>
       {children}
     </button>
   )
