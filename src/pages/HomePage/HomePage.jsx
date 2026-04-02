c78b08a3911709bd9cd60bddf95812f8bdf3010b
Sabyrka <sabyrka23@gmail.com>
2026-04-02 23:49:48 +0500

    feat: add QuestionCard component to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
+import { QuestionCard } from '@/components/QuestionCard'
 import styles from './HomePage.module.css'
 
 export const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <h1>Home</h1>
+      <QuestionCard />
     </div>
   )
 }
