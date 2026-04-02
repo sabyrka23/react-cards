cfaaca1b6d3e45b5e27a219bdfacb1a73259b9fb
Sabyrka <sabyrka23@gmail.com>
2026-04-02 23:48:10 +0500

    feat: replace Home component with HomePage in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MainLayout } from './components/MainLayout'
+import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route index element={<h1>Home</h1>} />
+          <Route index element={<HomePage />} />
           <Route path="add-question" element={<h1>Add Question</h1>} />
           <Route path="login" element={<h1>Login</h1>} />
         </Route>
