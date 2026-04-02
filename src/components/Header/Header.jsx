e924b5cbfafded7b1f98120bed3b9e0e79554088
Sabyrka <sabyrka23@gmail.com>
2026-04-02 23:11:41 +0500

    feat: update Header component to use SVG logo and improve accessibility

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
-import logoImage from '@/assets/logo.svg'
 import { Button } from '../Button'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Link className={styles.logo} to="/">
-        <img src={logoImage} alt="" width={44} height={44} />
-        <span>React Cards</span>
+      <Link className={styles.logo} to="/" title="Home">
+        <div className={styles.logoImage}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="-11.5 -10.23174 23 20.46348">
+            <title>React Logo</title>
+            <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
+            <g stroke="#61dafb" strokeWidth="1" fill="none">
+              <ellipse rx="11" ry="4.2" />
+              <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+              <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+            </g>
+          </svg>
+        </div>
+        <span className={styles.logoText}>
+          <span>React</span>
+          <span>Cards</span>
+        </span>
       </Link>
 
       <div className={styles.actions}>
