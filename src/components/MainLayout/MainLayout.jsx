c2556b56fe138d9f07378dfd922d3aef3d2e13ce
Sabyrka <sabyrka23@gmail.com>
2026-04-02 22:48:36 +0500

    feat: use Header

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Footer } from '../Footer'
 import styles from './MainLayout.module.css'
+import { Header } from '../Header'
 
 export const MainLayout = () => {
   return (
     <div className={styles.mainLayout}>
-      <header className={styles.header}>header</header>
+      <Header />
       <div className={styles.content}>
         <main className={styles.main}>
           <Outlet />
