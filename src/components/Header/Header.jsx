c8b1661b7a206b9f376d97bea6f020392c20cb48
Sabyrka <sabyrka23@gmail.com>
2026-04-02 22:57:16 +0500

    feat: add Header component with logo and action buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import logoImage from '@/assets/logo.svg'
+import { Button } from '../Button'
 
 export const Header = () => {
-  return <header className={styles.header}>header</header>
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} to="/">
+        <img src={logoImage} alt="" width={44} height={44} />
+        <span>React Cards</span>
+      </Link>
+
+      <div className={styles.actions}>
+        <Button>Add</Button>
+        <Button>Login</Button>
+      </div>
+    </header>
+  )
 }
