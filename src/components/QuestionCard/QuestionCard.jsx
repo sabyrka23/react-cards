9310fa7ad61385e7b7c0bf0c6d8d7a094bbc6dfc
Sabyrka <sabyrka23@gmail.com>
2026-04-02 23:49:15 +0500

    feat: add QuestionCard component with styles and export

---
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -0,0 +1,9 @@
+import styles from './QuestionCard.module.css'
+
+export const QuestionCard = () => {
+  return (
+    <div className={styles.questionCard}>
+      <h1>Question Card</h1>
+    </div>
+  )
+}
