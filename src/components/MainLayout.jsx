ae303dfa58b5b24848d62497e6c1b2d08892a805
Sabyrka <sabyrka23@gmail.com>
2026-04-02 20:56:23 +0500

    feat: implement MainLayout component with dynamic year

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,5 +1,23 @@
-import './MainLayout.module.css'
+import styles from './MainLayout.module.css'
 
 export const MainLayout = () => {
-  return <div>MainLayout</div>
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <div className={styles.mainLayout}>
+      <header className={styles.header}>header</header>
+      <div className={styles.content}>
+        <main className={styles.main}>main</main>
+        <footer className={styles.footer}>
+          <span>React Question Cards Application | {currentYear}</span>
+          <span>
+            Developed by{' '}
+            <a href="https://github.com/sabyrka23" target="_blank">
+              Sabyrka
+            </a>
+          </span>
+        </footer>
+      </div>
+    </div>
+  )
 }
