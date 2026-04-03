68fed3f042cd911c2fc31540bcadee0b5a26e974
Sabyrka <sabyrka23@gmail.com>
2026-04-03 14:29:19 +0500

    feat: create Button component

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import styles from './NotFoundPage.module.css'
+
+export const NotFoundPage = () => {
+  return (
+    <div className={styles.notFoundPage}>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
