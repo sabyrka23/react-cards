525d6cfa2c2c8c785d762691523ada5596252066
Sabyrka <sabyrka23@gmail.com>
2026-04-03 13:21:57 +0500

    feat: implement dynamic loading of QuestionCard components from dataset

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,29 @@
 import { QuestionCard } from '@/components/QuestionCard'
 import styles from './HomePage.module.css'
+import { useEffect, useState } from 'react'
 
 export const HomePage = () => {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await fetch('/db.json')
+      const data = await response.json()
+
+      setCards(data.react)
+    }
+
+    loadData()
+  }, [])
+
   return (
     <div className={styles.homePage}>
       <h1>Home</h1>
-      <QuestionCard />
+      <div className={styles.cards}>
+        {cards.map((card) => (
+          <QuestionCard key={card.id} {...card} />
+        ))}
+      </div>
     </div>
   )
 }
